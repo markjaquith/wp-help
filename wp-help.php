95b82ed2d65fa0b995da6ce392cf7ce4c0690d0c
Mark Jaquith <m@rkj.me>
2012-07-22 03:05:54 +0000

    Not done with 1.1 yet.

--- wp-help.php
+++ wp-help.php
@@ -2,7 +2,7 @@
 /*
 Plugin Name: WP Help
 Description: Administrators can create detailed, hierarchical documentation for the site's authors and editors, viewable in the WordPress admin.
-Version: 1.1
+Version: 1.1-beta-2
 License: GPL
 Plugin URI: http://txfx.net/wordpress-plugins/wp-help/
 Author: Mark Jaquith
