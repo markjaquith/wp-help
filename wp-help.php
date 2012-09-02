4f74ac3d42b57777b3a4dc0b463c8e0b024acc5a
Mark Jaquith <m@rkj.me>
2012-09-02 07:45:52 +0000

    Move to the .js/.min.js scheme as per WP 3.5 standard instead of the old .dev.js/.js standard.

--- wp-help.php
+++ wp-help.php
@@ -607,7 +607,7 @@ class CWS_WP_Help_Plugin {
 	}
 
 	public function enqueue() {
-		$suffix = defined ('SCRIPT_DEBUG' ) && SCRIPT_DEBUG ? '.dev' : '';
+		$suffix = defined ('SCRIPT_DEBUG' ) && SCRIPT_DEBUG ? '' : '.min';
 		wp_enqueue_style( 'cws-wp-help', plugins_url( "css/wp-help$suffix.css", __FILE__ ), array(), '20120721b' );
 		wp_enqueue_script( 'cws-wp-help', plugins_url( "js/wp-help$suffix.js", __FILE__ ), array( 'jquery', 'jquery-ui-sortable' ), '20120829' );
 		do_action( 'cws_wp_help_load' ); // Use this to enqueue your own styles for things like shortcodes.
