401935797f8ab090368318e9d98c612d41edf0bd
Mark Jaquith <mark@jaquith.me>
2013-01-14 15:08:05 -0500

    Align the Compass config.rb file

--- config.rb
+++ config.rb
@@ -1,7 +1,7 @@
-css_dir = "css"
-sass_dir = "css"
-images_dir = "images"
+css_dir         = "css"
+sass_dir        = "css"
+images_dir      = "images"
 javascripts_dir = "js"
 
 relative_assets = true
-output_style = :compressed
+output_style    = :compressed
