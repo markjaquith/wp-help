f6a2c93d63aed2b634f01ac31fa12a182338eaa1
Mark Jaquith <mark@jaquith.me>
2013-12-05 01:57:57 -0500

    Use `dashicons-editor-help` as the menu icon in WordPress 3.8-RC1 and above.
    fixes #13

--- wp-help.php
+++ wp-help.php
@@ -555,10 +555,12 @@ class CWS_WP_Help_Plugin extends WP_Stack_Plugin {
 	}
 
 	public function admin_menu() {
-		if ( 'dashboard-submenu' != $this->get_option( 'menu_location' ) )
-			$hook = add_menu_page( $this->get_option( 'h2' ), $this->get_option( 'h2' ), $this->get_cap( 'read_posts' ), self::MENU_SLUG, array( $this, 'render_listing_page' ), plugin_dir_url( __FILE__ ) . 'images/icon-16.png' );
-		else
+		if ( 'dashboard-submenu' != $this->get_option( 'menu_location' ) ) {
+			$icon = version_compare( $GLOBALS['wp_version'], '3.8-RC1', '>=' ) ? 'dashicons-editor-help' : plugin_dir_url( __FILE__ ) . 'images/icon-16.png';
+			$hook = add_menu_page( $this->get_option( 'h2' ), $this->get_option( 'h2' ), $this->get_cap( 'read_posts' ), self::MENU_SLUG, array( $this, 'render_listing_page' ), $icon );
+		} else {
 			$hook = add_dashboard_page( $this->get_option( 'h2' ), $this->get_option( 'h2' ), $this->get_cap( 'read_posts' ), self::MENU_SLUG, array( $this, 'render_listing_page' ) );
+		}
 		$this->hook( "load-{$hook}", 'enqueue' );
 	}
 
